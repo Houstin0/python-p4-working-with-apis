72c0c1992846d35eeb5546a0aac4c4fd543b4271
Houstin Angwenyi <houstin.angwenyi@student.moringaschool.com>
2023-09-22 07:37:35 +0300

    completed assignment

--- lib/nyc_api.py
+++ lib/nyc_api.py
@@ -9,7 +9,43 @@ class GetPrograms:
 
     response = requests.get(URL)
     return response.content
+  
+  def program_school(self):
+    program_list=[]
+    programs=json.loads(self.get_programs())
+    for program in programs:
+      program_list.append(program["agency"])
+
+      return program_list
 
 
-programs = GetPrograms().get_programs()
-print(programs)
+# programs = GetPrograms().get_programs()
+# print(programs)
+
+programs=GetPrograms()
+programs_schools=programs.program_school()
+
+for school in set(programs_schools):
+  print(school)
+
+
+#for any url
+
+class JSONLoader:
+  
+  def __init__(self,url):
+    self.url=url
+
+  def get_response_body(self):
+    response=requests.get(self.url)
+    return response.content
+
+  def load_json(self):
+    response_body=self.get_response_body()
+    return json.loads(response_body)
+
+url = "http://data.cityofnewyork.us/resource/uvks-tn5n.json"
+loader = JSONLoader(url)
+programs_schools = loader.load_json() 
+for program in programs_schools:
+  print(program.get("agency"))
